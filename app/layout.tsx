37fd833800be0aad90bf43f89e510cae21c76447
Alexander Popadyuk <al.popadyuk@gmail.com>
2025-08-28 10:48:35 +0200

    feat: registration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth-context";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
